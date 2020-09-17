2ee680e7532a44cc3a2a549b4ca9d87e2333df25
lalag <lalagvanan@gmail.com>
2020-09-17 22:30:30 +0300

    Now it works

---
+++ src/AuthenticationService.Validation/UserLoginValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using LT.DigitalOffice.AuthenticationService.Models.Dto;
+
+namespace LT.DigitalOffice.AuthenticationService.Validation
+{
+    public class UserLoginValidator : AbstractValidator<UserLoginInfoRequest>
+    {
+        public UserLoginValidator()
+        {
+            RuleFor(user => user.Email)
+                .NotEmpty();
+
+            RuleFor(user => user.Password)
+                .NotEmpty();
+        }
+    }
+}
