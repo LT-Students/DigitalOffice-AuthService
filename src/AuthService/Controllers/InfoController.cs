280513a0f6b4605d70291f6f093b25df12a632b8
KostinP <>
2021-04-04 17:59:51 +0300

    Create InfoController.cs

---
+++ src/AuthService/Controllers/InfoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LT.DigitalOffice.AuthService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class InfoController
+    {
+        private readonly string _apiVersion = "1.2.2";
+
+        [HttpGet]
+        public string GetApiInformation()
+        {
+            return _apiVersion;
+        }
+    }
+}
