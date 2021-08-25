2071a2cd653f2adcf036e165e920279295d3ceca
BelehovEgor <61228367+BelehovEgor@users.noreply.github.com>
2021-08-25 10:38:15 +0300

    add editorconfig (#33)
    * added editorconfig
    
    * fix

--- src/AuthService.Broker/Consumers/CheckTokenConsumer.cs
+++ src/AuthService.Broker/Consumers/CheckTokenConsumer.cs
@@ -9,24 +9,24 @@ using LT.DigitalOffice.AuthService.Models.Dto.Enums;
 namespace LT.DigitalOffice.AuthService.Broker.Consumers
 {
   public class CheckTokenConsumer : IConsumer<ICheckTokenRequest>
+  {
+    private readonly ITokenValidator _tokenValidator;
+
+    public CheckTokenConsumer(ITokenValidator tokenValidator)
     {
-        private readonly ITokenValidator _tokenValidator;
-
-        public CheckTokenConsumer(ITokenValidator tokenValidator)
-        {
-            _tokenValidator = tokenValidator;
-        }
-
-        public async Task Consume(ConsumeContext<ICheckTokenRequest> context)
-        {
-            var response = OperationResultWrapper.CreateResponse(GetValidationResult, context.Message);
-
-            await context.RespondAsync<IOperationResult<Guid>>(response);
-        }
-
-        private Guid GetValidationResult(ICheckTokenRequest request)
-        {
-            return _tokenValidator.Validate(request.Token, TokenType.Access);
-        }
+      _tokenValidator = tokenValidator;
     }
+
+    public async Task Consume(ConsumeContext<ICheckTokenRequest> context)
+    {
+      var response = OperationResultWrapper.CreateResponse(GetValidationResult, context.Message);
+
+      await context.RespondAsync<IOperationResult<Guid>>(response);
+    }
+
+    private Guid GetValidationResult(ICheckTokenRequest request)
+    {
+      return _tokenValidator.Validate(request.Token, TokenType.Access);
+    }
+  }
 }
