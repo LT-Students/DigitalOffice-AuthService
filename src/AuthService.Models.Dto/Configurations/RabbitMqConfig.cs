312406aec3fdf983a67333fbb1db6655c3108bc9
Mariya <63921256+MariyaSin@users.noreply.github.com>
2021-11-26 17:29:21 +0300

    merge from develop (#40)
    * Release/alpha 0.0.1 (#37)
    
    * init (#30)
    
    * Bug/add trim to user.login (#32)
    
    * Add login trim to LoginCommand
    
    * delete trim() drom validator
    
    * models version update
    
    * remove baget (#34)
    
    * add editorconfig (#33)
    
    * added editorconfig
    
    * fix
    
    Co-authored-by: Igor Likhachev <Fdiii@mail.ru>
    Co-authored-by: Nikita Doriomedov <88382379+ndoriomedov@users.noreply.github.com>
    Co-authored-by: BelehovEgor <61228367+BelehovEgor@users.noreply.github.com>
    
    * add getting rabbit mq credentials from env (#38)
    
    * add getting rabbit mq credentials from env
    
    * fix
    
    * update to .net6 (#39)
    
    Co-authored-by: Igor Likhachev <Fdiii@mail.ru>
    Co-authored-by: Nikita Doriomedov <88382379+ndoriomedov@users.noreply.github.com>
    Co-authored-by: BelehovEgor <61228367+BelehovEgor@users.noreply.github.com>

--- src/AuthService.Models.Dto/Configurations/RabbitMqConfig.cs
+++ src/AuthService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -4,10 +4,13 @@ using LT.DigitalOffice.Models.Broker.Requests.User;
 
 namespace LT.DigitalOffice.AuthService.Models.Dto.Configurations
 {
-    public class RabbitMqConfig : BaseRabbitMqConfig
-    {
-        [AutoInjectRequest(typeof(IGetUserCredentialsRequest))]
-        public string GetUserCredentialsEndpoint { get; set; }
-        public string GetTokenEndpoint { get; set; }
-    }
+  public class RabbitMqConfig : BaseRabbitMqConfig
+  {
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    [AutoInjectRequest(typeof(IGetUserCredentialsRequest))]
+    public string GetUserCredentialsEndpoint { get; set; }
+    public string GetTokenEndpoint { get; set; }
+  }
 }
